4445dd19c45c71c31f32f60280eefe202ee191cf
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-04-30 13:09:04 +0300

    Navbar and Modal WIP

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState, useEffect, useCallback } from "react";
+
+type Props = {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
+};
+
+const Modal = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryActionLabel,
+}: Props) => {
+  const [showModal, setShowModal] = useState(isOpen);
+
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    } else {
+      setShowModal(false);
+
+      // delaying closing of modal to show animation
+      setTimeout(() => {
+        onClose();
+      }, 300);
+    }
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) return null;
+
+  return <div>Modal</div>;
+};
+
+export default Modal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,34 @@
-import {Nunito} from 'next/font/google'
+import { Nunito } from "next/font/google";
 
-import './globals.css'
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
-  title: 'Airbnb Clone',
-  description: 'Full Stack Airbnb Clone with Next.js 13 App Router: React, Tailwind, Prisma, MongoDB, NextAuth 2023',
-}
+  title: "Airbnb Clone",
+  description:
+    "Full Stack Airbnb Clone with Next.js 13 App Router: React, Tailwind, Prisma, MongoDB, NextAuth 2023",
+};
 
 const font = Nunito({
-  subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal isOpen />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
