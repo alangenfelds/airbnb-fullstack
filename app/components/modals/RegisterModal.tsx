e25e7d3dd27581ec1890c3b7b6cdf2f981a87552
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-05-02 10:54:48 +0300

    Register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -87,6 +87,23 @@ const RegisterModal = (props: Props) => {
         icon={FcGoogle}
         onClick={() => {}}
       />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex items-center justify-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   );
 
