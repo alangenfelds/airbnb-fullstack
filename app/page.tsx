6589a6c9175b63b2a5638057d113fa5b2c32eef8
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-05-05 17:32:10 +0300

    Listings WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-type Props = {}
+type Props = {};
 
-const Home = (props: Props) => {
+const Home = async (props: Props) => {
+  const listings = await getListings();
+  const isEmpty = !listings.length;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'>Home</div>
-  )
-}
+    <ClientOnly>
+      <Container>
+        <div
+          className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
+        lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8
+        "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard key={listing.id} />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
+};
 
-export default Home
+export default Home;
